318a6f687bdbc130864563205cd904007bdfbf62
bek <bekrye@gmail.com>
2023-09-10 00:00:03 +0200

    Add useContext hook to navbar and app.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext)
+
+  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.navLink}
+        activeClassName={styles.active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.navLink}
+        activeClassName={styles.active}
+      >
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
+
   return (
     <Navbar className={styles.navbar} expand="md" fixed="top">
       <Container>
@@ -15,14 +38,11 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <NavLink exact className={styles.navLink} activeClassName={styles.active} to="/"><i className="fas fa-home"></i>Home
+            <NavLink exact className={styles.navLink} activeClassName={styles.active} to="/"><i className="fas fa-home"></i>
+            Home
             </NavLink>
-            <NavLink className={styles.navLink} activeClassName={styles.active} to="/signin">
-            <i className="fas fa-sign-in-alt"></i>Sing in
-            </NavLink>
-            <NavLink className={styles.navLink} activeClassName={styles.active} to="/signup">
-            <i className="fas fa-user-plus"></i>Sing up
-            </NavLink>
+
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 
 import Form from "react-bootstrap/Form";
@@ -14,32 +14,36 @@ import axios from "axios";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { SetCurrentUserContext } from "../../App";
 
 function SignInForm() {
-    const [signInData, setSignInData] = useState({
-        username: "",
-        password: "",
-      });
-      const { username, password } = signInData;
-      const [errors, setErrors] = useState({});
+  const setCurrentUser = useContext(SetCurrentUserContext);
 
-      const history = useHistory();
-      const handleChange = (event) => {
-        setSignInData({
-          ...signInData,
-          [event.target.name]: event.target.value,
-        });
-      };
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = signInData;
+  const [errors, setErrors] = useState({});
 
-      const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-          await axios.post("/dj-rest-auth/login/", signInData);
-          history.push("/");
-        } catch (err) {
-          setErrors(err.response?.data);
-        }
-      };
+  const history = useHistory();
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
 
   return (
     <Row className={styles.Row}>
@@ -48,24 +52,26 @@ function SignInForm() {
           <h1 className={styles.Header}>sign in</h1>
           <Form onSubmit={handleSubmit}>
             <Form.Group controlId="username">
-              <Form.Label className="d-none" >Username</Form.Label>
-              <Form.Control 
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
                 className={styles.Input}
                 type="text"
                 placeholder="Username"
                 name="username"
                 value={username}
-                onChange={handleChange} 
-                />
+                onChange={handleChange}
+              />
             </Form.Group>
-            {errors && errors.username && errors.username.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
+            {errors &&
+              errors.username &&
+              errors.username.map((message, idx) => (
+                <Alert variant="warning" key={idx}>
+                  {message}
+                </Alert>
+              ))}
 
             <Form.Group controlId="password">
-              <Form.Label className="d-none" >Password</Form.Label>
+              <Form.Label className="d-none">Password</Form.Label>
               <Form.Control
                 className={styles.Input}
                 type="password"
@@ -75,11 +81,13 @@ function SignInForm() {
                 onChange={handleChange}
               />
             </Form.Group>
-            {errors && errors.password && errors.password.map((message, idx) => (
-              <Alert key={idx} variant="warning">
-                {message}
-              </Alert>
-            ))}  
+            {errors &&
+              errors.password &&
+              errors.password.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                  {message}
+                </Alert>
+              ))}
 
             <Form.Group controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Check me out" />
@@ -90,11 +98,13 @@ function SignInForm() {
             >
               Sign in
             </Button>
-            {errors && errors.non_field_errors && errors.non_field_errors.map((message, idx) => (
-              <Alert key={idx} variant="warning" className="mt-3">
-                {message}
-              </Alert>
-            ))}
+            {errors &&
+              errors.non_field_errors &&
+              errors.non_field_errors.map((message, idx) => (
+                <Alert key={idx} variant="warning" className="mt-3">
+                  {message}
+                </Alert>
+              ))}
           </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
