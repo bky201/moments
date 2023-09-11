6206589c40dfb795569d63c7101286341f1cfbe8
bek <bekrye@gmail.com>
2023-09-11 19:42:06 +0200

    Add Profile Data Context

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
+import { ProfileDataProvider } from "./contexts/ProfileDataContext";
 
 const root = document.getElementById("root");
 
@@ -11,8 +12,10 @@ const reactRoot = ReactDOM.createRoot(root);
 
 reactRoot.render(
   <Router>
-    <CurrentUserProvider>
-    <App />
-    </CurrentUserProvider>
-  </Router>
+      <CurrentUserProvider>
+        <ProfileDataProvider>
+          <App />
+        </ProfileDataProvider>
+      </CurrentUserProvider>
+    </Router>
 );
