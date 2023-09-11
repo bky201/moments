4e93c4eef26e08e0415d16d493b40cd1317515bd
bek <bekrye@gmail.com>
2023-09-11 13:20:10 +0200

    Add post page

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
               <Route exact path="/posts/create" render={() => <PostCreateForm />} />
+              <Route exact path="/posts/:id" render={() => <PostPage />} />
               <Route render={() => <p>Page not found!</p>} />
             </Switch>
           </Container>
